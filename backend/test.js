8918fce4ed3980177c56e1a180371721485727cf
Tharunoff <s.tharunsofficial@gmail.com>
2026-03-12 10:34:19 +0530

    fix: enable tsc outDir for Render deploy, clean up tracked build artifacts

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var dotenv_1 = require("dotenv");
-dotenv_1.default.config();
-var app = (0, express_1.default)();
-app.listen(5001, function () {
-    console.log("Listening on 5001");
-});
--- backend/test2.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
-app.listen(5002, () => console.log("Javascript Server Listening on 5002"));
